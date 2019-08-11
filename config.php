e248710f3fc94a918210aa8cda925d1cc7155489
Jovaughn Powell <jovaughnpowell@my.unt.edu>
2019-08-11 10:57:00 -0500

    card responsive class added, adding new weight started

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conn= mysqli_connect('localhost','','','WeightApp');
+$conn= mysqli_connect('localhost','','','');
 
 if(mysqli_connect_errno()){
 	echo 'Failed to connect'. mysqli_connect_errno();
